108b4bd9a45f78d655523986cfba2a36e2b0c2d2
ashwini more <more.ashg98@gmail.com>
2022-08-14 22:13:34 +0530

    flipcoin

--- Program.cs
+++ Program.cs
@@ -1,87 +1,55 @@
 ﻿using System;
-using System.Collections;
-using System.Linq;
-using System.Text;
 
-namespace Adressbook
+namespace FlipCoin
 {
-    class AddressProgram
 
 
+
+    class program
     {
-        static void Main()
-
+        static void Main(string[] args)
         {
-            ArrayList adress = new
-                ArrayList { };
-            Boolean menu = true;
-            double keychar;
-            do
+            string[] coin = { "heads", "tails" };
+            int user;
+            int heads = 0;
+            int tails = 0;
+            var rand = new Random();
+            const int faces = 2;
+
+            Console.Write("enter 1 to flip a coin, or 2 to leave: ");
+            user = Convert.ToInt32(Console.ReadLine());
+
+            while (user == 1)
             {
-                Console.WriteLine("Welcome to my address program");
-                Console.WriteLine("**************");
+                int second = rand.Next(faces);
+                if (second == 1)
+                {
+                    Console.WriteLine("coin landed on heads");
+                    heads += 1;
+                    Console.WriteLine($"you have landed on heads: {heads} time(s)");
+                    Console.WriteLine($"you have landed on tails: {tails} time(s)");
 
-                Console.WriteLine(" Plese Enter your Firstname");
-                string str1 = "Fastname";
-                str1 = Console.ReadLine();
-                string valueString1 = str1;
+                }
+                else
+                {
+                    Console.WriteLine("coin landed on tails");
+                    tails += 1;
+                    Console.WriteLine($"you have landed on heads: {heads} time(s)");
+                    Console.WriteLine($"you have landed on tails: {tails} time(s)");
+                }
 
-                Console.WriteLine(" Plese Enter your Lirstname");
-                string str2 = "Lastname";
-                str2 = Console.ReadLine();
-                string valueString2 = str2;
+                Console.WriteLine("enter 1 to flip again, or 2 to leave: ");
+                user = Convert.ToInt32(Console.ReadLine());
 
-
-                Console.WriteLine(" Plese Enter your Address");
-                string str3 = "address";
-                str3 = Console.ReadLine();
-                string valueString3 = str3;
-
-                Console.WriteLine(" Plese Enter your Phonenumber");
-                string str4 = "Phonenumber";
-                str4 = Console.ReadLine();
-                string valueString4 = str4;
-
-                Console.WriteLine(" Plese Enter your State");
-                string str5 = "State";
-                str5 = Console.ReadLine();
-                string valueString5 = str5;
-
-                Console.WriteLine(" Plese Enter your Email");
-                string str6 = "Email";
-                str6 = Console.ReadLine();
-                string valueString6 = str6;
-
-                Console.ReadLine();
-
-
-                Console.WriteLine("Firstname you entered: " + valueString1);
-
-                Console.WriteLine("Lirstname you entered: " + valueString2);
-
-
-                Console.WriteLine("Address you entered: " + valueString3);
-
-
-                Console.WriteLine("Phonenumber you entered: " + valueString4);
-
-
-                Console.WriteLine("State you entered: " + valueString5);
-
-
-                Console.WriteLine("Email you entered: " + valueString6);
-                Console.ReadLine();
-
-
-
-
-               
             }
-            while (menu == true);
+            Console.WriteLine("goodbye");
+
+
         }
-
-        
-
     }
-       
 }
+
+         
+    
+         
+       
